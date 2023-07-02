2a086d003f06f46571e5b701932088c6a9cf26b8
Sajib Adhikary <tosajibadhi@gmail.com>
2023-07-02 13:53:05 +0600

    Parent-child components communication
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

---
+++ js/components/AssignmentCreate.js
@@ -0,0 +1,29 @@
+export default {
+    template: `
+        <form @submit.prevent="submit">
+            <div class="border border-gray-600 text-black">
+               <input v-model="newAssignment" placeholder="New Assignment" class="p-2">
+                   
+               <button type="submit" class="bg-white p-2 border-l">Add</button>
+            </div>
+        </form>
+    `,
+
+    data(){
+        return {
+            newAssignment: '',
+        };
+    },
+
+    props: {
+        assignments: Array,
+    },
+
+    methods: {
+        submit(){
+            this.$emit('add', this.newAssignment);
+
+            this.newAssignment = '';
+        },
+    }
+}
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,19 +1,14 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 export default {
-    components: { AssignmentList },
+    components: { AssignmentList, AssignmentCreate },
 
     template: `
         <section class="space-y-6">
            <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
            <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
            
-            <form @submit.prevent="add">
-            <div class="border border-gray-600 text-black">
-               <input v-model="newAssignment" placeholder="New Assignment" class="p-2">
-               
-               <button type="submit" class="bg-white p-2 border-l">Add</button>
-            </div>
-            </form>
+           <assignment-create @add="add"></assignment-create>
         </section>
     `,
 
@@ -26,8 +21,6 @@ export default {
                 { id: 3, name: "Clean Room", complete: false },
                 { id: 4, name: "Walk Dog", complete: false },
             ],
-
-            newAssignment: '',
         };
     },
 
@@ -41,14 +34,12 @@ export default {
     },
 
     methods: {
-        add(){
+        add(name){
             this.assignments.push({
-                name: this.newAssignment,
+                name: name,
                 complete: false,
                 id: this.assignments.length + 1
             });
-
-            this.newAssignment = '';
         },
     }
 }
