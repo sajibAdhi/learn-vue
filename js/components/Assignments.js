d4087dce85f135984c43e1a62881a6b734d50c4c
Sajib Adhikary <tosajibadhi@gmail.com>
2023-07-02 13:32:25 +0600

    Handel a form submission
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -6,6 +6,14 @@ export default {
         <section class="space-y-6">
            <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
            <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+           
+            <form @submit.prevent="add">
+            <div class="border border-gray-600 text-black">
+               <input v-model="newAssignment" placeholder="New Assignment" class="p-2">
+               
+               <button type="submit" class="bg-white p-2 border-l">Add</button>
+            </div>
+            </form>
         </section>
     `,
 
@@ -17,7 +25,9 @@ export default {
                 { id: 2, name: "Read Chapter 4", complete: false },
                 { id: 3, name: "Clean Room", complete: false },
                 { id: 4, name: "Walk Dog", complete: false },
-            ]
+            ],
+
+            newAssignment: '',
         };
     },
 
@@ -28,5 +38,17 @@ export default {
                 completed: this.assignments.filter(a => a.complete)
             };
         }
+    },
+
+    methods: {
+        add(){
+            this.assignments.push({
+                name: this.newAssignment,
+                complete: false,
+                id: this.assignments.length + 1
+            });
+
+            this.newAssignment = '';
+        },
     }
 }
